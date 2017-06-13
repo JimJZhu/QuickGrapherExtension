5d093813d06e1e7adfcd47a5c0cb53a054e9b1bf
Jim Zhu <flappyboby@gmail.com>
2017-06-13 12:40:30 -0400

    Added error checking and inproved visuals.

--- src/js/popup.js
+++ src/js/popup.js
@@ -3,38 +3,51 @@ function click() {
     var y = document.getElementById("yValues").value;
     var ctx = document.getElementById("myChart").getContext('2d');
     var message = document.getElementById("message");
-    var strXArray = x.split(',');
-    var strYArray = y.split(',');
-    if (strXArray.length != strYArray.length) {
-        message.innerHTML = "Wrong number of values.";
-        return -1;
-    } else {
-        message.innerHTML = "Generating...";
-    }
-    var userData = [];
-    for (i = 0; i < strXArray.length; i++) {
-        var point = {};
-        point.x = parseInt(strXArray[i]);
-        point.y = parseInt(strYArray[i]);
-        userData.push(point);
-    }
-    var myChart = new Chart(ctx, {
-        type: 'scatter',
-        data: {
-            datasets: [{
-                label: 'Scatter Data',
-                data: userData,
-            }]
-        },
-        options: {
-            scales: {
-                xAxes: [{
-                    type: 'linear',
-                    position: 'bottom'
-                }]
-            }
+    message.innerHTML = "Generating Graph...";
+    try {
+        var strXArray = x.split(',');
+        var strYArray = y.split(',');
+        if (strXArray.length != strYArray.length) {
+            throw "Wrong number of values.";
         }
-    });
+        var userData = [];
+        for (i = 0; i < strXArray.length; i++) {
+            var point = {};
+            var xInt = parseInt(strXArray[i]);
+            var yInt = parseInt(strYArray[i]);
+            if (isNaN(xInt) || isNaN(yInt)) {
+                throw "Invalid values: (" + strXArray[i] + ", " + strYArray[i] + ").";
+            }
+            point.x = xInt;
+            point.y = yInt;
+            userData.push(point);
+        }
+        var myChart = new Chart(ctx, {
+            type: 'scatter',
+            data: {
+                datasets: [{
+                    label: 'Scatter Data',
+                    data: userData,
+                    fill: false,
+                    pointRadius: 5,
+                    pointBorderWidth: 2,
+                    pointBackgroundColor: 'rgba(65, 184, 131, 0.4)',
+                    pointBorderColor: 'rgba(16, 90, 30, 0.9)',
+                }]
+            },
+            options: {
+                scales: {
+                    xAxes: [{
+                        type: 'linear',
+                        position: 'bottom'
+                    }]
+                },
+                showLines: false,
+            }
+        });
+    } catch (err) {
+        message.innerHTML = err;
+    } finally {}
 }
 
 document.addEventListener('DOMContentLoaded', function() {
